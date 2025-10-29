6a43b39bcbb8b515cefc27c9178fd2c1e6f9ad35
kelleysinning <kelley.sinning@gmail.com>
2025-10-29 12:31:02 -0600

    SI!

---
+++ SI21-24.R
@@ -0,0 +1,94 @@
+# BVR SI Data for Kelley Sinning from May 2021 to present
+
+# Load packages
+library(tidyverse)
+library(SIBER)
+library(lme4)
+library(lmerTest)
+library(pbkrtest)
+library(emmeans)
+library(car)
+library(kableExtra)
+
+setwd("~/Library/CloudStorage/OneDrive-Colostate/Data/BVR")
+
+# IMPORT SIA DATA
+SIA_FISH <- read_csv("Meta_SIA_Data.csv")
+
+str(SIA_FISH)
+
+SIA_FISH
+
+# C:N vs d13C SCATTER PLOT
+ggplot(SIA_FISH, aes(x=CN, y=d13C)) +
+  geom_point() +
+  geom_smooth(method = "lm", se=T,formula=y~x) 
+  #facet_grid(Species ~ Occasion)
+
+# C:N BOXPLOT
+ggplot(SIA_FISH, aes(x=Species, group = Species, y=CN, colour=Species)) +
+  geom_boxplot() +
+  geom_point(position="jitter", size = 0.75, shape = 1)+
+  #ylim(3,5) +
+  geom_hline(aes(yintercept=4), colour="red") +
+  facet_grid(. ~ Occasion) +
+  theme_minimal()
+
+# PROPORTION OF FISH UNDER 3.5 C:N THRESHOLD
+SIA_FISH %>%
+  summarize(proportion = sum(CN < 3.5) / n()) %>% 
+  kable(caption = "% C:N < 3.5", 
+        booktabs= T, 
+        digits=2) 
+
+# MEAN C:N FOR BROWN TROUT & MOTTLED SCULPIN
+SIA_FISH %>%
+  group_by(Species) %>%
+  summarise('Mean C:N' = mean(CN)) %>% 
+  kable(caption = "MEAN LIPID CONTENT (C:N)", 
+        booktabs= T, 
+        digits=2) 
+
+
+
+
+
+# FORMAT FOR SIBER
+data_SIA_SIBER <- SIA_FISH %>%
+  select(d13C, d15N, Species, Occasion) %>%
+  rename("iso1" = "d13C",
+         "iso2" = "d15N",
+         "group" = "Species",
+         "community" = "Occasion") %>%
+  filter(Species == "BNT", Species == "MTS")
+  as.data.frame()
+
+  
+  data_SIA_SIBER <- SIA_FISH %>%
+    select(d13C, d15N, Species, Occasion) %>%
+    rename(
+      iso1 = d13C,
+      iso2 = d15N,
+      group = Species,
+      community = Occasion
+    ) %>%
+    filter(group %in% c("BNT", "MTS")) %>%
+    as.data.frame()
+  
+# CREATE SIBER OBJECT
+SIA_SIBER_OBJECT <- createSiberObject(data_SIA_SIBER)
+
+# SIBER PLOT
+ggplot(data_SIA_SIBER, aes(x = iso1, y = iso2, colour = group)) +
+  geom_point(size = 1.5, shape = 1) +
+  stat_ellipse(position="identity", geom = "polygon", aes(fill = group), level=0.4, linewidth = 1, alpha = 0.25)+
+  stat_ellipse(position="identity", level=0.95, linewidth=0.5, linetype = 2) +
+  facet_wrap(. ~ community) +
+  xlab(expression(paste(delta^{13}, "C (\u2030)"))) +
+  ylab(expression(paste(delta^{15}, "N (\u2030)"))) +
+  scale_color_manual(values = c("#E69F00", "#0072B2")) +
+  scale_fill_manual(values = c("#E69F00", "#0072B2")) +
+  theme_bw() +
+  theme(legend.title = element_blank(),
+        legend.position = c(0.05, 0.95),
+        legend.justification = c("left", "top"))
