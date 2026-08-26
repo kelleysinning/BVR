45cca14307666c89052cc7fbddf98bd2e334c7fa
kelleysinning <kelley.sinning@gmail.com>
2026-08-26 08:37:57 -0600

    SEM

--- SEM.R
+++ SEM.R
@@ -1,10 +1,17 @@
 # SEM code for Fall 2026
 
 install.packages("jagsUI")
+library(jagsUI)
 install.packages("rstan")
-install.packages("RTools46")
+library(rstan)
+install.packages("rtools")
+library(rtools)
 install.packages("vioplot")
+library(vioplot)
 install.packages("lavaan")
+library(lavaan)
 install.packages("piecewiseSEM")
+library(piecewiseSEM)
 install.packages("blavaan")
+library(blavaan)
 
